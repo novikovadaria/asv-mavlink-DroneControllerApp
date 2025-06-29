5c1eee5ed3cb240a4b042d63ebfcc2b589aa357b
Daria N <smb_udk@vk.com>
2025-06-29 05:45:41 +0300

    automatization

--- View/ConsolePositionObserver.cs
+++ View/ConsolePositionObserver.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-namespace DroneControllerApp.View
+﻿namespace DroneControllerApp.View
 {
     public class ConsolePositionObserver : IPositionObserver
     {
